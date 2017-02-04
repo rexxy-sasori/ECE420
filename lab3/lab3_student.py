02e26e792e12a6494088ef9c1c950688e4ae7cc5
kakashi-hadakei <963136786@qq.com>
2017-02-04 09:58:37 -0600

    updated 420 directory

--- lab3/lab3_student.py
+++ lab3/lab3_student.py
@@ -2,25 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import read, write
 from numpy.fft import fft, ifft
+from scipy import signal
 
 FRAME_SIZE = 1024
 ZP_FACTOR = 2
 FFT_SIZE = FRAME_SIZE * ZP_FACTOR
+scale = 10
 
 
 ################## YOUR CODE HERE ######################
 def ece420ProcessFrame(frame):
     curFft = np.zeros(FFT_SIZE)
+    window_frame = signal.hamming(FRAME_SIZE) * frame
+    curFft = fft(window_frame,n = FFT_SIZE)
+    curFft = curFft[0:int(FFT_SIZE/2)]
+
+    curFft = np.log10(np.square((np.absolute(curFft))))/scale
 
     return curFft
 
-
 ################# GIVEN CODE BELOW #####################
 
-Fs, data = read('test_vector.wav')
+Fs, data = read('test_single_tones.wav')
 
 numFrames = int(len(data) / FRAME_SIZE)
-bmp = np.zeros((numFrames, FFT_SIZE / 2))
+bmp = np.zeros((numFrames, int(FFT_SIZE / 2)))
 
 for i in range(numFrames):
     frame = data[i * FRAME_SIZE : (i + 1) * FRAME_SIZE]
