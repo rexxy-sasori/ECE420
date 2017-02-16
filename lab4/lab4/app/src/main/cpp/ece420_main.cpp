83b84c28eda69496bd96987e836770f81263ccc7
kakashi-hadakei <963136786@qq.com>
2017-02-16 15:29:30 -0600

    lab5 update

--- lab4/lab4/app/src/main/cpp/ece420_main.cpp
+++ lab4/lab4/app/src/main/cpp/ece420_main.cpp
@@ -7,7 +7,7 @@
 #include "kiss_fft/kiss_fft.h"
 #include "audio_common.h"
 
-#define VOICED_THRESHOLD 123456789 // Write your own!
+#define VOICED_THRESHOLD 50000000 // Write your own!
 
 // TODO: better initialization, this is not great
 float lastFreqDetected = -1;
@@ -53,7 +53,57 @@ void ece420ProcessFrame(sample_buf *dataBuf) {
     // Finally, write the variable "lastFreqDetected" on completion. If voiced,
     // write your determined frequency. If unvoiced, write -1.
     // ********************** START YOUR CODE HERE ************************ //
-    
+
+    float Es = 0;
+    for(int i = 0; i < FRAME_SIZE; i++)
+    {
+        Es += abs(bufferIn[i]*bufferIn[i]);
+    }
+
+    if(Es > VOICED_THRESHOLD)
+    {
+        kiss_fft_cfg cfg = kiss_fft_alloc(FRAME_SIZE,0,NULL,NULL);
+        kiss_fft_scalar zero;
+        memset(&zero,0,sizeof(zero));
+
+        kiss_fft_cpx cx_in[FRAME_SIZE] = {};
+        kiss_fft_cpx cx_out[FRAME_SIZE] = {};
+        for(int k = 0;k<FRAME_SIZE;k++)
+        {
+            //put kth sample in cx_in[k].r and cx_in[k].i
+            cx_in[k].r = bufferIn[k];
+        }
+
+        kiss_fft(cfg,cx_in,cx_out);
+
+        for(int k = 0;k<FRAME_SIZE;k++)
+        {
+            cx_out[k].r = pow(cx_out[k].r,2) + pow(cx_out[k].i,2);
+            cx_out[k].i = 0;
+        }
+
+        kiss_fft_cfg cfgi = kiss_fft_alloc(FRAME_SIZE,1,NULL,NULL);
+        kiss_fft(cfgi,cx_out,cx_in);
+
+        //a = cx_in;
+        for(int k = 0;k<FRAME_SIZE;k++)
+        {
+            //put kth sample in cx_in[k].r and cx_in[k].i
+            bufferIn[k] = cx_in[k].r;
+        }
+        int L = findMaxArrayIdx(bufferIn,50,FRAME_SIZE/2);
+
+        float Fs = 48000;
+
+        lastFreqDetected = Fs/L;
+
+
+        free(cfg);
+        free(cfgi);
+
+    }
+    else
+        lastFreqDetected = -1;
 
     // ************************ END YOUR CODE HERE ************************ //
 
--- lab4/lab4_student.py
+++ lab4/lab4_student.py
@@ -9,19 +9,40 @@ FRAME_SIZE = 2048
 def ece420ProcessFrame(frame, Fs):
     freq = -1
 
+    Es = np.sum(np.square(np.absolute(frame)))
+
+    THRESH = 3000000000
+    if(Es > THRESH):
+        X = np.fft.fft(frame)
+        A = np.square(np.absolute(X))
+        a = np.fft.ifft(A)
+
+        max = -np.inf
+        l = 0
+        for i in range(50,int(FRAME_SIZE/2)):
+            if(np.absolute(a[i]) > max):
+                max = a[i]
+                l = i
+
+        if(l != 0):
+            freq = Fs/l
+
     return freq
+
 
 
 ################# GIVEN CODE BELOW #####################
 
 Fs, data = read('test_vector.wav')
 
+print(len(data))
 numFrames = int(len(data) / FRAME_SIZE)
 frequencies = np.zeros(numFrames)
 
 for i in range(numFrames):
     frame = data[i * FRAME_SIZE : (i + 1) * FRAME_SIZE]
     frequencies[i] = ece420ProcessFrame(frame.astype(float), Fs)
+
 
 plt.figure()
 plt.plot(frequencies)
